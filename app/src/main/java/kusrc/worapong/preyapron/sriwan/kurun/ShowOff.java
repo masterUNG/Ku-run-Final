082ac3b84b4e4e415f600c2837a9f2c6dbcd8b86
masterUNG <phrombutr@gmail.com>
2016-05-31 22:12:54 +0700

    Finish

--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/ShowOff.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/ShowOff.java
@@ -1,9 +1,10 @@
 package kusrc.worapong.preyapron.sriwan.kurun;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.squareup.okhttp.OkHttpClient;
@@ -11,10 +12,13 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ShowOff extends AppCompatActivity {
 
     private ListView listView;
+    private String[] nameStrings, avateStrings, goldStrings;
+    private int[] avataInts, goldInts;
 
 
     @Override
@@ -23,6 +27,9 @@ public class ShowOff extends AppCompatActivity {
         setContentView(R.layout.activity_show_off);
 
         listView = (ListView) findViewById(R.id.listView2);
+
+        ConnectedUser connectedUser = new ConnectedUser();
+        connectedUser.execute();
 
     }   // Main Method
 
@@ -50,16 +57,55 @@ public class ShowOff extends AppCompatActivity {
 
             try {
 
+                Log.d("31MayV1", "JSoN ==> " + s);
+
                 JSONArray jsonArray = new JSONArray(s);
 
+                nameStrings = new String[jsonArray.length()];
+                avateStrings = new String[jsonArray.length()];
+                goldStrings = new String[jsonArray.length()];
+                avataInts = new int[jsonArray.length()];
+                goldInts = new int[jsonArray.length()];
 
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    nameStrings[i] = jsonObject.getString("Name");
+                    avateStrings[i] = jsonObject.getString("Avata");
+                    avataInts[i] = createAvata(avateStrings[i]);
+                    goldStrings[i] = jsonObject.getString("Gold");
+                    goldInts[i] = createGold(goldStrings[i]);
+
+
+
+                }
+
+                ShowOffAdapter showOffAdapter = new ShowOffAdapter(ShowOff.this, avataInts, goldInts, nameStrings);
+                listView.setAdapter(showOffAdapter);
 
 
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.d("31MayV2", "e ==> " + e.toString());
             }
 
         }   // onPost
     }   // Class
+
+    private int createGold(String goldString) {
+
+        int[] resultInts = new int[]{R.drawable.image0,
+                R.drawable.image1,R.drawable.imge2, R.drawable.image3};
+
+        return resultInts[Integer.parseInt(goldString)];
+    }
+
+    private int createAvata(String avateString) {
+
+        int[] resultInts = new int[]{R.drawable.kon48,
+                R.drawable.rat48, R.drawable.bird48, R.drawable.doremon48, R.drawable.nobita48};
+
+        return resultInts[Integer.parseInt(avateString)];
+    }
 
 }   // Main Class
